8c28e3069e146dd60cc96c8ffe080046f9fe29f5
David Freitag <freitagdavid@outlook.com>
2019-04-09 00:54:40 -0400

    queue/queue: Complete

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,18 @@
+from linkedlist import Linked_List
+
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = Linked_List()
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.append(item)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        return self.storage.del_head()
+
+    def len(self):
+        return len(self.storage)
