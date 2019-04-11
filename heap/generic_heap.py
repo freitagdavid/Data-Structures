e7839a623196d074e003e01dd672898749ac6e64
David Freitag <freitagdavid@outlook.com>
2019-04-11 17:56:48 -0400

    heap/generic_heap: Removed unnecessary comment

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,27 +1,3 @@
-# class Heap:
-#   def __init__(self, comparator):
-#     self.storage = []
-#     self.comparator = comparator
-
-#   def insert(self, value):
-#     pass
-
-#   def delete(self):
-#     pass
-
-#   def get_priority(self):
-#     pass
-
-#   def get_size(self):
-#     pass
-
-#   def _bubble_up(self, index):
-#     pass
-
-#   def _sift_down(self, index):
-#     pass
-
-
 class Heap:
     def __init__(self, comparator=lambda x, y: x > y):
         self.storage = []
